a6bbb346c0cff78e8d34f56dc4b0705b509932f8
SUJIN <amor77777777778@gmail.com>
2019-04-29 23:53:37 +0900

    불필요한 주석 제거

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import ListView from '../views/ListView.vue';
-//import TodoList from '../components/TodoList.vue';
 
 Vue.use(VueRouter)
 
